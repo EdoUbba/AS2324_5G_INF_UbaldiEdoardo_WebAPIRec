f2b24d0e2ee2b3853379cf89fe4a46eb505e1da9
Edoardo Ubaldi <Edoubba@gmail.com>
2024-05-15 08:39:07 +0200

    feat: add celsius_to_fahrenheit()

--- AS2324_5G_INF_UbaldiEdoardo_WebAPIRec/Controllers/MathController.cs
+++ AS2324_5G_INF_UbaldiEdoardo_WebAPIRec/Controllers/MathController.cs
@@ -7,5 +7,15 @@ namespace AS2324_5G_INF_UbaldiEdoardo_WebAPIRec.Controllers
     [ApiController]
     public class MathController : ControllerBase
     {
+        [HttpGet("celsius_to_fahrenheit")]
+        public JsonResult celsius_to_fahrenheit(double celsius)
+        {
+            return new JsonResult(
+                new
+                {
+                    risultato = (celsius * 9 / 5) + 32,
+                });
+        }
+
     }
 }
