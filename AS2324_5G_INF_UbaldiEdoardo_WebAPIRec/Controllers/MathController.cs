0cb43a77e98e63cca3cb20d74aeaf8d3eda14ecf
Edoardo Ubaldi <Edoubba@gmail.com>
2024-05-15 08:30:52 +0200

    Aggiungere i file di progetto.

---
+++ AS2324_5G_INF_UbaldiEdoardo_WebAPIRec/Controllers/MathController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_UbaldiEdoardo_WebAPIRec.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+    }
+}
